338a9bf116bb63a2c2ebb8270bfdfe6256f302e3
Mp-keyner <keyneroswaldo0@gmail.com>
2023-10-02 23:39:28 -0500

    2 de octubre 11:39

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -1,4 +1,4 @@
-import { Box, Button } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
 
 const NotFound = () => {
@@ -10,22 +10,21 @@ const NotFound = () => {
                 width: '100%',
                 display: 'flex',
                 alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'column'
+                justifyContent: 'flex-end',
+                flexDirection: 'column',
+                backgroundImage: 'url("https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/documentos%2Ffondo404.png?alt=media&token=617b1f32-2405-432a-ad89-7db287b28bb3")',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
             }}>
-                <Box
-                    component={'img'}
-                    src='https://firebasestorage.googleapis.com/v0/b/fb-picporter.appspot.com/o/documentos%2F404Fy.png?alt=media&token=82f1a6f4-6d10-499d-9586-18a27c96a4b1'
-                    alt='404'
-                    sx={{
-                        width: '100%',
-                        'md': {
-                            width: '50%'
-                        }
 
-                    }}
-                />
-                <Button href='https://finances-roan.vercel.app/'>Go to Home</Button>
+                <Typography variant='h4' sx={{
+                    color: '#fe553e'
+                }}>Page not found</Typography>
+                <Button href='https://finances-roan.vercel.app/' sx={{
+                    marginBottom: '4pc',
+                    border: '1px solid #fe553e',
+                    color: '#fe553e'
+                }}>Go to Home</Button>
             </Box>
         </>
     )
